bdb6aabc98cc52047517eaf0a0d108a602dc401c
Ruzeen <50741941+b1ackh01e-pitch@users.noreply.github.com>
2020-12-16 13:08:53 +0200

    test
    test

---
+++ Assets/Scripts/AimDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimDestroy : MonoBehaviour
+{
+    [SerializeField] private float stamina = 8f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > stamina)
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
---
+++ Assets/Scripts/CoreBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreBehaviour : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D CoreRigid;
+    [SerializeField] private bool isPressed = false;
+    [SerializeField] public Rigidbody2D ShootRigid;
+    [SerializeField] private float maxDistance = 2f;
+    [SerializeField] public GameObject CorePrefab;
+    [SerializeField] public Transform CoreSpawnerPos;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CoreRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isPressed == true)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(mousePos, ShootRigid.position) > maxDistance)
+            {
+                CoreRigid.position = ShootRigid.position + (mousePos - ShootRigid.position).normalized * maxDistance;
+
+            }
+            else
+            {
+                CoreRigid.position = mousePos;
+            }
+        }
+    }
+        private void OnMouseDown()
+        {
+            isPressed = true;
+            CoreRigid.isKinematic = true;
+        }
+
+        private void OnMouseUp()
+        {
+            isPressed = false;
+            CoreRigid.isKinematic = false;
+
+            StartCoroutine(LetGo());
+        }
+
+        IEnumerator LetGo()
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            gameObject.GetComponent<SpringJoint2D>().enabled = false;
+            this.enabled = false;
+
+            yield return new WaitForSeconds(1);
+
+            if (CorePrefab != null)
+            {
+            CorePrefab.transform.position = CoreSpawnerPos.position;
+            }
+        }
+}
